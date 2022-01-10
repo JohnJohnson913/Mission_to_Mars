ecf6491c2246b7eca90c2d48de9b4ccccb8a5ef3
JohnJohnson913 <88688549+JohnJohnson913@users.noreply.github.com>
2022-01-10 16:57:24 -0600

    Updating app.py file for real this time :)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
-import scraping
+import Scraping
+app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
@@ -18,7 +19,7 @@ def scrape():
    mars.update_one({}, {"$set":mars_data}, upsert=True)
    return redirect('/', code=302)
 
-.update_one(query_parameter, {"$set": data}, options)
+#.update_one(query_parameter, {"$set": data}, options)
 
 if __name__ == "__main__":
    app.run()
